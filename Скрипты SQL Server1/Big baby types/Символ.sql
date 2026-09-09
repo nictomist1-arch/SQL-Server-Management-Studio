fd31c59f49e5b19a703cba5b4ea60b318dc96bee
HoMaX <nicto.mist1@gmail.com>
2026-09-09 15:25:02 +0300

    1

--- Скрипты SQL Server1/Big baby types/Символ.sql
+++ Скрипты SQL Server1/Big baby types/Символ.sql
@@ -1,7 +1,8 @@
 DECLARE @char char(5) = 'dddddddd'
 DECLARE @char2 nchar(10) = 'текст на русском'
 DECLARE @vchar varchar(5000) = 'dadasaasda'
-DECLARE @text text = 'sdsdasfasdvzcxzas'
+
+--DECLARE @text text = 'sdsdasfasdvzcxzas'
 
 SELECT *
 FROM sys.databases
